08308c8ece2e7ff6d63718538f2235314f009c8b
Gabriel Falcao <gabriel@nacaolivre.org>
2014-01-12 23:22:24 -0500

    refactoring handy functions

--- bong/framework/handy/functions.py
+++ bong/framework/handy/functions.py
@@ -48,14 +48,6 @@ EMPTYGEO = {
 }
 
 
-def get_distance(loc_1, loc_2):
-    return (1 * math.sqrt(
-        math.pow(
-            (69.1 * (loc_1.latitude - loc_2.latitude)), 2) +
-        math.pow(
-            (53 * (loc_1.longitude - loc_2.longitude)), 2)))
-
-
 def geo_data_for_ip(ip_address):
     geoip = PyGEOIP(settings.GEO_IP_FILE_LOCATION)
     try:
@@ -66,18 +58,9 @@ def geo_data_for_ip(ip_address):
 
 
 def get_ip():
-    try:
-        if request.headers.getlist("X-Real-IP"):
-            ip = request.headers.get("X-Real-IP")
-            logger.info("Retrieved IP %s from header X-Real-IP", ip)
-        elif not request.headers.getlist("X-Forwarded-For"):
-            ip = request.remote_addr
-            logger.info("Retrieved IP %s from request.remote_addr", ip)
-        else:
-            ip = request.headers.getlist("X-Forwarded-For")[0]
-            logger.info("Retrieved IP %s from header X-Forwarded-For", ip)
+    if request.headers.getlist("X-Real-IP"):
+        return request.headers.get("X-Real-IP")
+    elif request.headers.getlist("X-Forwarded-For"):
+        return request.headers.getlist("X-Forwarded-For")[0]
 
-        return ip
-    except:
-        logger.exception("Error retrieving ip address")
-        return request.remote_addr
+    return request.remote_addr
